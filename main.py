5e064dff0f72a41f64289755cc16016a8cc37495
ioant <ioan.tivig@gmail.com>
2023-03-26 21:37:25 +0300

    OpenDEP v1.3 - small changes 1

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-# ------------------------------------------------------
-# ---------------------- main.py -----------------------
-# ------------------------------------------------------
+'''
+    ------------------------------------------------------
+    --------------------- MAIN FILE ----------------------
+    ------------------------------------------------------
+OpenDEP
+    Copyright (C) 2022  Ioan Cristian Tivig
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    You can contact the developer/owner of OpenDEP at "ioan.tivig@gmail.com".
+'''
 
 ### START IMPORTS ###
 ### Local imports ###
@@ -740,4 +759,15 @@ trayIcon.setIcon(QIcon("icon.png"))
 trayIcon.setVisible(True)
 
 window.show()
+
+print('''
+OpenDEP  Copyright (C) 2022  Ioan Cristian Tivig
+This program comes with ABSOLUTELY NO WARRANTY
+This is free software, and you are welcome to redistribute it
+under certain conditions; See LICENSE file in root folder for more info
+You can contact the developer/owner of OpenDEP at "ioan.tivig@gmail.com".
+'''
+    )
+
 app.exec_()
+
